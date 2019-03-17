409ea6043d98e459402d302bb65621aa1081293e
mt <d.rong@outlook.com>
2019-03-17 18:26:57 +0800

    fix decode error

--- shadowray/common/B64.py
+++ shadowray/common/B64.py
@@ -3,11 +3,9 @@ import base64
 
 def decode(text):
     n = len(text)
-    if n % 3 is 1:
-        text += "=="
+    missing_padding = 4 - n % 4
 
-    if n % 3 is 2:
-        text += "="
-
+    if missing_padding:
+        text += '=' * missing_padding
     text = bytes(text, encoding='utf8')
     return str(base64.b64decode(text), encoding='utf8')
